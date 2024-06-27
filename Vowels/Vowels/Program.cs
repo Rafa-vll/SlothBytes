ee4088d6ac786035f889edfeda79552f4d20ab9f
Rafaela Valle <101656006+Rafa-vll@users.noreply.github.com>
2024-06-27 16:48:36 -0300

    Add files via upload

---
+++ Vowels/Vowels/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Vowels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+            string word2 = Console.ReadLine();
+            string word = word2.ToLower();
+            
+
+            int vowelCount = CountVowels(word);
+
+            Console.WriteLine($"Number of vowels in '{word2}': {vowelCount}");
+        }
+
+        static int CountVowels(string str)
+        {
+            int count = 0;
+            string vowels = "aeiou";
+
+            foreach (char c in str)
+            {
+                if (vowels.Contains(c))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
