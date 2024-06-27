f81308a909810c52f8fcb4f4aa65b0013d9a0d53
Rafaela Valle <101656006+Rafa-vll@users.noreply.github.com>
2024-06-27 14:00:14 -0300

    Add files via upload

---
+++ Making Box/Making Box/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Channels;
+
+namespace Making_Box
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entar a dimenson of the box: ");
+            int dimension = int.Parse(Console.ReadLine());
+
+            List<string> box = MakeBox(dimension);
+
+            PrintBox(box);
+        }
+
+        static List<string> MakeBox(int n)
+        {
+            List<string> box = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i == 0 || i == n - 1)
+                {
+                    string line = new string('#', n);
+                    box.Add(line);
+                }
+                else
+                {
+                    string line = '#' + new string(' ', n - 2) + '#';
+                    box.Add(line);
+                }
+            }
+            return box;
+        }
+
+        static void PrintBox(List<string> box)
+        {
+            foreach (string line in box)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine();
+        }
+    }
+}
